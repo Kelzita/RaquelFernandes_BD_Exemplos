a6ae4fbf5a52e2b900a6087e8ebbbe0c88c6396e
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-04-11 14:01:20 -0300

    Add

--- Atividades Banco de Dados/Atividade 12/Create Table Item.sql
+++ Atividades Banco de Dados/Atividade 12/Create Table Item.sql
@@ -4,7 +4,7 @@ create table item (
   Cod_Produto integer,
   Quantidade integer,
   
-  constraint pk_Item primary key(Cod_Item),
+  constraint pk_Item primary key(Cod_Item, Cod_Compra,Cod_Produto),
   constraint fk_Cod_Compra foreign key(Cod_Compra) references Compra(Cod_Compra),
   constraint fk_Cod_Produto foreign key(Cod_Produto) references Produto(Cod_Produto)
 
