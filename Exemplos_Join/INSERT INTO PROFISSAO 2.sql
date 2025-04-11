2958c21c9b739192666bf0d94be03561581c4a9c
raquelfernandes <raquelfernandesbriyoidjdj@gmail.com>
2025-04-11 16:25:27 -0300

    Add

---
+++ Exemplos_Join/INSERT INTO PROFISSAO 2.sql
@@ -0,0 +1,5 @@
+INSERT INTO Profissao (
+Cod_Profissao,
+Nome_Profissao)
+VALUES 
+  (29, 'Web Designer');
---
+++ Exemplos_Join/SELECT_FROM_RIGHT OUTER JOIN_ON.sql
@@ -0,0 +1,2 @@
+SELECT * FROM Cliente1 
+RIGHT OUTER JOIN Profissao ON Cliente1.Cod_Profissao = Profissao.Cod_Profissao;
